5bc5fe14bf92b038cd1bd8785383d76488535081
alkesh <118254569+alkesh6394@users.noreply.github.com>
2024-05-13 10:57:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ def sum_of_two_numbers(num1, num2):
     return num1 + num2
 
 # Define numbers
-num1 = 50
-num2 = 30
+num1 = 
+num2 = 
 
 # Calculate sum using the function
-result = sum_of_two_numbers(num1, num2)
-
 # Print the result
 print("Sum of", num1, "and", num2, "is:", result)
